95a60b47cc8c99fd112beeb6fb12093234a6860d
sinaTejeh <amrjv1378@gmail.com>
2026-06-07 19:34:53 +0330

    create header component and out source it outside the app folder

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function About() {
     return (
         <main>
             <h1>About Us :)</h1>
+            <Link href='../'>Back</Link>
         </main>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
+import Header from "@/components/header.js";
+
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Header />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p><Link href="/about">About us</Link></p>
     </main>
